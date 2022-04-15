7b706c7f2a051937d83eabc262308f789756ba29
GuanLu <guanlug@163.com>
2022-04-15 17:11:49 +0800

    add club lineup function

--- club.py
+++ club.py
@@ -1,3 +1,4 @@
+import random
 from copy import copy
 import base_class
 import character
@@ -46,10 +47,14 @@ class Club(base_class.BaseClub):
         self.__descript = kwargs.copy()
 
     def __str__(self):
-        return f"{self.__class__.__name__}({self.__name})"
+        return "<{}({}), {}>".format(
+            self.__class__.__name__, self.__name, self.players
+        )
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.__name})"
+        return "<{}({}), {}>".format(
+            self.__class__.__name__, self.__name, self.players
+        )
 
     def __iter__(self):
         return ((k, v) for k, v in self.description().items())
@@ -59,6 +64,16 @@ class Club(base_class.BaseClub):
 
     def __bool__(self):
         return bool(self.__name)
+
+    @property
+    def locations(self):
+        """返回球员各个位置的精确名称"""
+        return {
+            "后卫": ['左后卫', '后卫', '右后卫'],
+            "中场": ['中场', '前腰', '中卫', '后腰', '中前卫'],
+            "前锋": ['前锋', '中锋', '左边锋', '右边锋'],
+            "守门员": ['守门员']
+        }
 
     @property
     def name(self) -> str:
@@ -139,13 +154,107 @@ class Club(base_class.BaseClub):
                 return True
         return False
 
+    def query_players(self, by=character.PlayersAttr.CH_NAME, value=None):
+        """查询球员; 根据球员的某一个属性进行判断,返回第一个匹配到的球员"""
+        self_players = self.__descript.get(ClubAttr.PLAYERS)
+        if not self_players:
+            return None
+        if isinstance(self_players, character.Players):
+            if eval(f"self_players.{by}") == value:
+                return self_players
+        for players in self.__descript.get(ClubAttr.PLAYERS):
+            if eval(f"players.{by}") == value:
+                return players
+
+    def starting_lineup(self, guard=4, midfield=4, forward=2):
+        """返回11位首发球员; 如果当前俱乐部存的所有球员不足11位则抛出异常
+        球员阵型:
+            "442": 4 后卫, 4 中场, 2 前锋
+            "451": 4 后卫, 5 中场, 1 前锋
+            "433": 4 后卫, 3 中场, 3 前锋
+            ...
+        如果当前俱乐部球员不够以固定位置安排阵型时,则会随机使用其他位置球员进行占位
+        :param guard: 后卫球员人数
+        :param midfield: 中场球员人数
+        :param forward: 前锋球员人数
+        :return: list
+        """
+        if sum([guard, midfield, forward]) != 10:
+            raise ValueError(
+                "The parameter value is unreasonable. The total number should be 10"
+            )
+        if len(self.players) < 11:
+            raise AttributeError("The current number of players is less than 11")
+        return self._get_starting_lineup(guard, midfield, forward)
+
+    def get_players_by_location(self, location: list, quantity=1):
+        """根据location中的球员位置返回指定数量的球员
+        如果筛选出的球员不够指定数量时会抛出AttributeError
+
+        Used:
+            location = Club().locations['前锋']
+            Club().get_players_by_location(location, quantity=2)
+
+        :param location: 球员位置
+        :param quantity: 球员数量
+        :return: list
+        """
+        select = self._get_location_players(location, quantity)
+        if len(select) < quantity:
+            raise AttributeError(f"currently club less than {quantity} players")
+        return select
+
+    def _get_location_players(self, location: list, quantity: int):
+        total_players = self.players
+        if not total_players:
+            return list()
+        random.shuffle(total_players)
+        filter_ = list()
+        for players in total_players:
+            if len(filter_) >= quantity:
+                break
+            if players.location in location:
+                filter_.append(copy(players))
+        return filter_
+
+    def _get_starting_lineup(self, guard: int, midfield: int, forward: int):
+        filter_, lack = list(), 0
+        for locations, quantity in zip(self.locations.values(), [guard, midfield, forward, 1]):
+            select = self._get_location_players(locations, quantity)
+            if len(select) < quantity:
+                lack += (quantity - len(select))
+            filter_.extend(select)
+        if lack == 0:
+            return filter_
+        for p in self.players:
+            if lack == 0:
+                break
+            if p not in filter_:
+                filter_.append(p)
+                lack -= 1
+        return filter_
+
+    def get_random_lineup(self):
+        """返回11位任意位置的球员"""
+        if not self.players:
+            return None
+        random.shuffle(self.players)
+        players_list = list()
+        for players in self.players:
+            if len(players_list) >= 11:
+                break
+            if players not in players_list:
+                players_list.append(copy(players))
+        return players_list
+
 
 if __name__ == '__main__':
     from basedata import PLAYERS_DATABASE, COACH_DATABASE
     import pprint
+
     manchester_united = [
         '万·比萨卡', '克里斯蒂亚诺·罗纳尔多', '博格巴', '卡瓦尼', '卢克·肖', '布鲁诺·费尔南德斯', '弗雷德', '拉什福德', '拜利', '杰登·桑乔',
-        '林德洛夫', '格林伍德', '特莱斯', '瓦拉内', '范德贝克', '达洛特', '马夏尔', '马奎尔', '马蒂奇', '麦克托米奈', '林加德']
+        '林德洛夫', '格林伍德', '特莱斯', '瓦拉内', '范德贝克', '达洛特', '马夏尔', '马奎尔', '马蒂奇', '麦克托米奈', '林加德', '德赫亚']
 
     description = {
         "city": "曼彻斯特",
@@ -157,4 +266,6 @@ if __name__ == '__main__':
         "league_matches": "英超",
     }
     club = Club("曼彻斯特联", **description)
-    pprint.pprint(club.description())
+    pprint.pprint(club.starting_lineup())
+
+
